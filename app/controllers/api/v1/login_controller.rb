ad76b04e477e8e04b0d51a6ac57d07b2e6ab4e34
Gangadhar Swami <gangadhar.mswami@gmail.com>
2019-09-05 13:59:59 +0530

    fixed the result of login api

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::LoginController < Api::ApiController
   #skip_before_action :authenticate, only: [:sign_in]
 
   def sign_in
-    @student = Student.where(parent_mobile_number: params[:parentMobile], roll_number: params[:rollNumber])
+    @student = Student.find_by(parent_mobile_number: params[:parentMobile], roll_number: params[:rollNumber])
     @student.present? ? (render status: :ok) : (render status: :bad_request)
   end
   
