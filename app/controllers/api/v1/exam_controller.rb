8d012a076d0edb185f5eaad096b87fadc7836bf7
Gangadhar Swami <gangadhar.mswami@gmail.com>
2019-09-06 14:21:27 +0530

    changed test date format

--- app/controllers/api/v1/exam_controller.rb
+++ app/controllers/api/v1/exam_controller.rb
@@ -2,11 +2,10 @@ class Api::V1::ExamController < Api::ApiController
 
   def list_of_past_exams
     student_id = params[:student_id]
-    @past_exams = Test.joins(:test_students).where("test_students.student_id = ? and tests.test_date < ?", student_id, Date.today).select("tests.*, test_students.*")
+    @past_exams = Test.joins(:test_students).where("test_students.student_id = ? and tests.test_date < ?", student_id, Date.today).select("tests.*, test_students.*, to_char(tests.test_date, 'dd/mm/yyyy') as formatted_test_date")
   end
 
   def list_of_upcoming_exams
-    student_id = params[:student_id]
-    @upcoming_exams = Test.where("tests.test_date > ?", Date.today)
+    @upcoming_exams = Test.where("tests.test_date > ?", Date.today).select("tests.*, to_char(test_date, 'dd/mm/yyyy') as formatted_test_date")
   end
 end
