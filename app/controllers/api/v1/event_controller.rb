336b7ba2bb1d0cb25f4dc363e12eb58c2ddb74bb
Gangadhar Swami <gangadhar.mswami@gmail.com>
2019-09-09 12:30:59 +0530

    event date format changed in events

--- app/controllers/api/v1/event_controller.rb
+++ app/controllers/api/v1/event_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::EventController < Api::ApiController
 
   def list_of_events
-    @events = Event.all
+    @events = Event.select("events.*, to_char(event_date, 'dd/mm/yyyy') as formatted_event_date")
   end
 end
