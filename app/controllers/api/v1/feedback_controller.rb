2e87d8d87e546e387a26eb2821ebb8c7daf023da
Gangadhar Swami <gangadhar.mswami@gmail.com>
2019-09-11 13:14:00 +0530

    added json view for gallery

--- app/controllers/api/v1/feedback_controller.rb
+++ app/controllers/api/v1/feedback_controller.rb
@@ -5,16 +5,16 @@ class Api::V1::FeedbackController < Api::ApiController
     @response[:status] ? (render status: :ok) : (render status: :bad_request)
   end
 
-   def get_gallery
-    images = []
+  def get_gallery
+    @images = []
     (1..17).each do |index|
-      images << { image: "https://#{request.host}#{ActionController::Base.helpers.asset_path("#{index}.jpg")}" }
+      @images << { image: "https://#{request.host}#{ActionController::Base.helpers.asset_path("#{index}.jpg")}" }
     end
 
     (1..15).each do |i|
       index = "0#{i}"
-      images << { image: "https://#{request.host}#{ActionController::Base.helpers.asset_path("#{index}.jpg")}" }
+      @images << { image: "https://#{request.host}#{ActionController::Base.helpers.asset_path("#{index}.jpg")}" }
     end
-    render json: images
+    
   end
 end
