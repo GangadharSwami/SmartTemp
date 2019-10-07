611fb07501ce0565ac00f0fa06701f34af17f95e
GangadharSwami <gangadhar.mswami@gmail.com>
2019-10-07 12:52:55 +0530

    changed past exams api

--- app/controllers/api/v1/exam_controller.rb
+++ app/controllers/api/v1/exam_controller.rb
@@ -1,12 +1,44 @@
 class Api::V1::ExamController < Api::ApiController
 
+  # def list_of_past_exams
+  #   @student_id = params[:student_id].to_i
+  #   @past_exams = Test.joins(:test_students).where("test_students.student_id = ? and tests.test_date < ?", @student_id, Date.today).
+  #     select("tests.*, test_students.*, to_char(tests.test_date, 'dd/mm/yyyy') as formatted_test_date")
+  #   @past_exams.each do |exam|
+  #     exam.rank = get_rank(exam.test_id)
+  #   end 
+  #   @past_exams = @past_exams.map{ |exam|
+  #     {
+  #       id: exam.id,
+  #       name: exam.name,
+  #       description: exam.description,
+  #       test_date: exam.formatted_test_date,
+  #       no_of_questions: exam.no_of_questions,
+  #       total_marks: exam.total_marks,
+  #       passing_marks: exam.passing_marks,
+  #       answer_key: exam.answer_key,
+  #       is_theory: exam.is_theory,
+  #       is_combined: exam.is_combined,
+  #       question_paper_link: exam.question_paper_link,
+  #       answer_paper_link: exam.answer_paper_link,
+  #       created_at: exam.created_at,
+  #       test_id: exam.test_id,
+  #       student_marks: exam.student_marks,
+  #       student_answer_key: exam.student_answer_key,
+  #       rank: exam.rank,
+  #       test_duration: '45 min',
+  #       test_time: '10.00 AM',
+  #       is_student_present: true
+  #     }
+  #   }  
+  # end
+
   def list_of_past_exams
     @student_id = params[:student_id].to_i
-    @past_exams = Test.joins(:test_students).where("test_students.student_id = ? and tests.test_date < ?", @student_id, Date.today).
-      select("tests.*, test_students.*, to_char(tests.test_date, 'dd/mm/yyyy') as formatted_test_date")
+    @past_exams = Test.joins(:test_students).where("test_students.student_id = ? and tests.test_date < ?", @student_id, Date.today).order(test_date: :desc).select("tests.*, test_students.*, to_char(tests.test_date, 'dd Mon yyyy') as formatted_test_date")
     @past_exams.each do |exam|
       exam.rank = get_rank(exam.test_id)
-    end 
+    end
     @past_exams = @past_exams.map{ |exam|
       {
         id: exam.id,
@@ -28,7 +60,8 @@ class Api::V1::ExamController < Api::ApiController
         rank: exam.rank,
         test_duration: '45 min',
         test_time: '10.00 AM',
-        is_student_present: true
+        is_student_present: true,
+        review_answers: answers_data(exam.answer_key, exam.student_answer_key)
       }
     }  
   end
@@ -59,6 +92,22 @@ class Api::V1::ExamController < Api::ApiController
   end
 
   private
+
+   def answers_data(answer_key, student_answer_key)
+    answer_key_arr = answer_key.split('')
+    student_key_arr = student_answer_key.split('')
+    q_count = 0
+    final_data = answer_key_arr.zip(student_key_arr).map do |modal_ans, student_ans|
+      q_count += 1
+      {
+        question_no: q_count,
+        modal_ans: modal_ans,
+        student_ans: student_ans,
+        marks: student_ans == "@" ? "-1" : (student_ans == modal_ans ? "+4" : "0" )
+      }
+    end
+    final_data
+  end
 
   def get_rank(test_id)
     tests = TestStudent.where(test_id: test_id).select('test_id, student_id, student_marks, rank() OVER (PARTITION BY test_id ORDER BY student_marks DESC) AS rank')
